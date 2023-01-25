a8949facc7b16ef026d75b2f998146489937fbed
rielgbrl <rielgasy9@gmail.com>
2023-01-25 12:44:03 +0700

    add meetup details

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,11 @@
+import { Fragment } from "react"
+import MeetupDetail from "components/meetups/MeetupDetail";
+function MeetupDetails(){
+    return <>
+        <h1>test</h1>
+        <MeetupDetail image="https://images.unsplash.com/photo-1661961112835-ca6f5811d2af?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1472&q=80" title='First Meetups' address='Some Address' description='desc'></MeetupDetail>
+    </>
+    
+}
+
+export default MeetupDetails
